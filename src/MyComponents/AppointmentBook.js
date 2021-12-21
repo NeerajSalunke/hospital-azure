e149fd374b59cf26103637b97a1f43c99dd64366
Neeraj <salunkeneeraj@gmail.com>
2021-12-21 22:17:17 +0530

    Added emailjs code

--- src/MyComponents/AppointmentBook.js
+++ src/MyComponents/AppointmentBook.js
@@ -1,9 +1,30 @@
-function AppointmentBook(){
-    return(
-        <>
-            <span>Book Appointment</span>
-        </>
-    )
-}
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
+export const AppointmentBook = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('contact_service', 'contact_form', form.current, 'user_kshYWY0DFRlLW7n4LBDdi')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
 export default AppointmentBook;
