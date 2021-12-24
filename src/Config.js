cabe7cb92b33dc1e0eeff36dbdbbb2e01cf2974e
Neeraj <salunkeneeraj@gmail.com>
2021-12-25 00:24:48 +0530

    added azure AD to Book Appointment

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,8 @@
 export const config = {
-    appId: '1b8e02eb-c107-4a1a-8876-66585648146b',
+    appId: 'f7581431-20f8-429b-a3bc-d9d0c2b8a0f5',
     redirectUri: 'http://localhost:3000/appointmentbook',
     scopes: [
         'user.read'
     ],
-    // authority:'https://login.microsoftonline.com/vit.edu.in'
+    authority:'https://login.microsoftonline.com/consumers/'
 };
