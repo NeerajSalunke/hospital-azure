2e82149106645385df6bcfa47774b50a7d6dd844
Neeraj <salunkeneeraj@gmail.com>
2021-12-27 15:00:16 +0530

    changed redirecturi

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,7 @@
 export const config = {
     appId: 'f7581431-20f8-429b-a3bc-d9d0c2b8a0f5',
-    redirectUri: 'http://localhost:3000/appointmentbook',
+    // redirectUri: 'http://localhost:3000/appointmentbook',
+    redirectUri: 'https://hospital-azure.azurewebsites.net/appointmentbook',
     scopes: [
         'user.read'
     ],
