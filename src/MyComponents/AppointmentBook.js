a90570c560240a83ef5b71e45fdac18342eb34e3
Neeraj <salunkeneeraj@gmail.com>
2021-12-29 00:05:53 +0530

    modified css of form

--- src/MyComponents/AppointmentBook.js
+++ src/MyComponents/AppointmentBook.js
@@ -126,7 +126,7 @@ function AppointmentBook() {
           <form ref={form} onSubmit={sendEmail}>
             <input className='formDetails' type="text" name="user_name" placeholder='Enter Your Name' />
             <input className='formDetails' type="email" name="user_email" placeholder='Enter your Email ID' />
-            <label htmlFor='date_time' className='formDetails'>To Select Date and time, Click on
+            <label htmlFor='date_time' className='formDetailsInstruction'>To Select Date and time, Click on
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar" viewBox="0 0 16 16">
                 <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
               </svg>
